ca1129822b80084f443dfdb253a5206019311387
Ryku <ryku7code@gmail.com>
2025-01-26 19:00:45 +0900

    add app server and proxy server

---
+++ src/bin/proxy.rs
@@ -0,0 +1,31 @@
+use async_trait::async_trait;
+use pingora::prelude::*;
+
+struct LB;
+
+#[async_trait]
+impl ProxyHttp for LB {
+    type CTX = ();
+    fn new_ctx(&self) -> Self::CTX {
+        ()
+    }
+
+    // リクエストを転送する先を決定する
+    async fn upstream_peer(&self, _session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
+        let peer = HttpPeer::new("127.0.0.1:3000", false, "".to_string());
+        Ok(Box::new(peer))
+    }
+}
+
+fn main() -> pingora::Result<()> {
+    env_logger::init();
+
+    let mut my_server = Server::new(None)?;
+    my_server.bootstrap();
+
+    let mut lb = http_proxy_service(&my_server.configuration, LB);
+    lb.add_tcp("[::]:8000");
+    my_server.add_service(lb);
+
+    my_server.run_forever();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::atomic::{AtomicU64, Ordering};
+
+use async_trait::async_trait;
+use http::header::{CONTENT_LENGTH, CONTENT_TYPE};
+use http::{Response, StatusCode};
+use pingora::apps::http_app::ServeHttp;
+use pingora::protocols::http::ServerSession;
+use pingora::server::Server;
+use pingora::services::listening::Service;
+
+struct HelloApp {
+    counter: AtomicU64,
+}
+
+#[async_trait]
+impl ServeHttp for HelloApp {
+    async fn response(&self, _server_session: &mut ServerSession) -> Response<Vec<u8>> {
+        let count = self.counter.fetch_add(1, Ordering::SeqCst);
+        let body = format!("Hello, World! あなたは{}人目の訪問者です。\n", count + 1);
+        Response::builder()
+            .status(StatusCode::OK)
+            .header(CONTENT_TYPE, "text/plain")
+            .header(CONTENT_LENGTH, body.len().to_string().as_str())
+            .body(body.into_bytes())
+            .unwrap()
+    }
+}
+
+fn main() -> pingora::Result<()> {
+    env_logger::init();
+    let mut server = Server::new(None)?;
+    server.bootstrap();
+
+    let hello_app = HelloApp {
+        counter: AtomicU64::new(0),
+    };
+    let mut hello_service = Service::new("hello_app".to_owned(), hello_app);
+    hello_service.add_tcp("[::]:3000");
+    server.add_service(hello_service);
+    server.run_forever();
 }
